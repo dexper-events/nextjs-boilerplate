3bf0e8a39f5fc8d158ba1448ece9b07ea9ade5e1
anass1008 <a.s@dexper.io>
2023-12-12 14:12:39 +0100

    added test page

---
+++ src/pages/test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import type { GetServerSideProps, NextPage } from "next";
+
+type Props = {
+  serverData: string;
+};
+
+/**
+ * The Test page
+ * @return {JSX.Element} The JSX Code for the page
+ */
+
+// eslint-disable-next-line react/prop-types
+const Test: NextPage<Props> = ({ serverData }) => {
+  return <h1>{serverData}</h1>;
+};
+
+/**
+ * SSR
+ * @return {Object} The Props to pass along to the front-end
+ */
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=300, stale-while-revalidate=604800"
+  );
+  res.setHeader(
+    "CDN-Cache-Control",
+    "s-maxage=300, stale-while-revalidate=604800"
+  );
+
+  return {
+    props: {
+      serverData: "server side props",
+    },
+  };
+};
+
+export default Test;
